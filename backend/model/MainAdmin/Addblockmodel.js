e56c57f497906f70800b813c6f18b67ba6dd32a3
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-11-27 23:15:16 +0530

    updated code

--- backend/model/MainAdmin/Addblockmodel.js
+++ backend/model/MainAdmin/Addblockmodel.js
@@ -16,7 +16,7 @@ const blockModel = {
   getFlatsName: async () => {
     const sql = "SELECT sid, society_name FROM Addsociety WHERE is_active = 1";
     const [rows] = await query(sql);
-    return rows;
+    return [rows];
   },
 
   getBlock: async () => {
