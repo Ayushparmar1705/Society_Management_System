8286f452c89b4fe6b5728cbe723ad17ba791037c
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-11-27 22:53:23 +0530

    updated code in Addblockmodal.js file

--- backend/model/MainAdmin/Addblockmodel.js
+++ backend/model/MainAdmin/Addblockmodel.js
@@ -1,30 +1,30 @@
 const { query } = require("../../config/dbConfig");
 
 const blockModel = {
-  addBlock: async (data) => {
+  addBlock: async (data,callback) => {
     const sql = "INSERT INTO Addblock(sid , block_name) VALUES(?,?)";
-    const [result] = await query(sql, [data.society_id, data.block_name]);
+    const [result] = await query(sql, [data.society_id, data.block_name],callback);
     return result;
   },
 
-  uniqueName: async (name) => {
+  uniqueName: async (name,callback) => {
     const sql = "SELECT * FROM Addblock WHERE block_name = ?";
-    const [rows] = await query(sql, [name]);
+    const [rows] = await query(sql, [name],callback);
     return rows;
   },
 
-  getFlatsName: async () => {
+  getFlatsName: async (callback) => {
     const sql = "SELECT sid, society_name FROM Addsociety WHERE is_active = 1";
-    const [rows] = await query(sql);
+    const [rows] = await query(sql,callback);
     return rows;
   },
 
-  getBlock: async () => {
+  getBlock: async (callback) => {
     const sql = `
       SELECT * FROM Addblock b
       INNER JOIN Addsociety s ON b.sid = s.sid
     `;
-    const [rows] = await query(sql);
+    const [rows] = await query(sql,callback);
     return rows;
   }
 };
