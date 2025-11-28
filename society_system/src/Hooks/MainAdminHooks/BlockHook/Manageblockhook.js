7722f258ee8db6ceb149b0fcfab94c201ba5311b
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-11-28 14:00:18 +0530

    update code to automatic get the block details when it's active or in active on button click in frontend

--- society_system/src/Hooks/MainAdminHooks/BlockHook/Manageblockhook.js
+++ society_system/src/Hooks/MainAdminHooks/BlockHook/Manageblockhook.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import ManageBlock from '../../../Api/MainAdmin/ManageBlock';
 import ManageBlockPage from '../../../Pages/MainAdmin/BlockManagement/ManageBlockPage';
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 export default function Manageblockhook() {
     const [blockList, setBlockList] = useState([]);
     const [duplicateBlock, setduplicateBlock] = useState([]);
-    const [societyList , setSocietyList] = useState([]);
+   
     const [isLoading, setIsLoading] = useState(true);
     const getBlocks = (async () => {
         try {
@@ -21,43 +21,42 @@ export default function Manageblockhook() {
             setIsLoading(false);
         }
     })
-        useEffect(() => {
+    useEffect(() => {
         getBlocks()
-        
+
     }, []);
 
 
-    const inActiveBlock = async(bid)=>{
-        try{
+    const inActiveBlock = async (bid) => {
+        try {
             const result = await ManageBlock.inActiveBlock(bid);
             console.log(result);
-            if(result.code === 500)
-            {
+            if (result.code === 500) {
                 toast.error(result.message);
-                getBlocks();
+                
             }
-            else
-            {
+            else {
+                ManageBlock.getBlock();
                 toast.success(result.message);
             }
 
-        }catch(err){
+        } catch (err) {
             console.log(err);
         }
     }
-    const ActiveBlock = async(bid)=>{
-        try{
+    const ActiveBlock = async (bid) => {
+        try {
             const result = await ManageBlock.ActiveBlock(bid);
-            if(result.code === 500)
-            {
+            if (result.code === 500) {
                 toast.error(result.message);
-                getBlocks();
+
+               
             }
-            else
-            {
+            else {
+                ManageBlock.getBlock();
                 toast.success(result.message);
             }
-        }catch(err){
+        } catch (err) {
             console.log(err);
         }
     }
