bf3c01df9d3cba32788d0406fe67881c9c887df6
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-11-28 18:21:16 +0530

    Updated Addflats controller

--- backend/controller/MainAdmin/Addflatscontroller.js
+++ backend/controller/MainAdmin/Addflatscontroller.js
@@ -2,41 +2,42 @@ const { flatsModel } = require("../../model/MainAdmin/Addflatsmodel");
 const flatsController = {
     // create the object to add new block in db
     Addflats: async (req, res) => {
-        // request by the client
-        const data = req.body;
-        console.log(data);
+        try {
+            const data = req.body;
+            console.log("Request Data:", data);
 
-
-
-        flatsModel.getBlocksById(data.bid, async (err, result) => {
-            if (err) {
-                return res.status(500).send({ code: 500, message: err });
+            // Get block by ID
+            const blockResultArr = await flatsModel.getBlocksById(data.bid);
+            if (!blockResultArr || blockResultArr.length === 0) {
+                return res.status(404).json({ code: 404, message: "Block not found" });
             }
-            const blockResult = result[0]["block_name"];
-            console.log(blockResult);
+
+            const blockName = blockResultArr[0].block_name;
+            console.log("Block Name:", blockName);
+
+            // Prepare new flat data
             const newdata = {
-                "sid": data.sid,
-                "bid": data.bid,
-                "flat_number": data.flat_number,
-                "floor_number": data.floor_number,
-                "flat_type": data.flat_type,
-                "flat_price": data.flat_price,
-                "flat_area": data.flat_area,
-                "balcony_area": data.balcony_area,
-                "flat_code": blockResult + " - " + data.flat_number,
-            }
+                sid: data.sid,
+                bid: data.bid,
+                flat_number: data.flat_number,
+                floor_number: data.floor_number,
+                flat_type: data.flat_type,
+                flat_price: data.flat_price,
+                flat_area: data.flat_area,
+                balcony_area: data.balcony_area,
+                flat_code: `${blockName} - ${data.flat_number}`
+            };
+
+            // Add flat
             const result1 = await flatsModel.addFlats(newdata);
-            console.log(result1);
-            try {
-                res.status(200).send({ code: 200, message: result1 });
-            } catch (err) {
-                console.log(err);
-                res.status(500).send({ code: 500, message: err });
-            }
-        })
+            console.log("Insert Result:", result1);
 
+            res.status(200).json({ code: 200, message: "Flat added successfully", data: result1 });
 
-
+        } catch (err) {
+            console.error("Error in Addflats:", err);
+            res.status(500).json({ code: 500, message: err.message });
+        }
     },
     // get the all society name from the database
     getSocietyName: async (req, res) => {
