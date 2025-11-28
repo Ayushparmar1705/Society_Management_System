9e6c945e28a7e54fb07508c3b79d484ec771e196
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-11-28 10:55:36 +0530

    Add block modal code updated in git

--- backend/model/MainAdmin/Addblockmodel.js
+++ backend/model/MainAdmin/Addblockmodel.js
@@ -2,14 +2,18 @@ const { query } = require("../../config/dbConfig");
 
 const blockModel = {
   addBlock: async (data) => {
+
+    console.log(data);
     const sql = "INSERT INTO Addblock(sid , block_name) VALUES(?,?)";
-    const [result] = await query(sql, [data.society_id, data.block_name]);
+
+    const result = await query(sql, [data.society_id, data.block_name]);
     return result;
   },
 
   uniqueName: async (name) => {
     const sql = "SELECT * FROM Addblock WHERE block_name = ?";
-    const [rows] = await query(sql, [name]);
+    // use the rows not [rows] any anywhere which is any variable because [] return first index
+    const rows = await query(sql, [name]);
     return rows;
   },
 
@@ -24,7 +28,7 @@ const blockModel = {
       SELECT * FROM Addblock b
       INNER JOIN Addsociety s ON b.sid = s.sid
     `;
-    const [rows] = await query(sql);
+    const rows = await query(sql);
     return rows;
   }
 };
