d171c60bd61ab6c1810a3fa2994db2dfbe4ddfc3
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-11-29 16:51:59 +0530

    change the get flats by id routes

--- society_system/src/Apiroutes/MainAdminRoutes/MainAdminEndpoints.js
+++ society_system/src/Apiroutes/MainAdminRoutes/MainAdminEndpoints.js
@@ -18,7 +18,7 @@ const MainAdminApiEndPoints = {
     "add_flats": `${BASE_URL}/mainadmin/addflats`,
     "getblockbyflatsname": (id) => `${BASE_URL}/mainadmin/getblockbyflatname/${id}`,
     "getFlats": (page, limit) => `${BASE_URL}/mainadmin/getFlats/${page}/${limit}`,
-    "getFlatById": (id) => `${BASE_URL}/mainadmin/getflatbyid/${id}`,
+    "getFlatById": (id) => `${BASE_URL}/mainadmin/getFlatById/${id}`,
     "update_society": (id) => `${BASE_URL}/mainadmin/updatesociety/${id}`,
 
 }
