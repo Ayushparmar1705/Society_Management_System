a21a0aafb6ebe769938bb4ca23a1a5b9375a0a04
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-11-29 20:45:26 +0530

    uplode the dashboard code

--- backend/model/RolebasedAuth/Chairman/Dashboard.js
+++ backend/model/RolebasedAuth/Chairman/Dashboard.js
@@ -1,14 +1,15 @@
-const conn = require("../../../config/dbConfig")
+const { query } = require("../../config/dbConfig");
+
 const Dashboardmodel = {
     countTotalMembers: (callback) => {
         // count total residence
         const sql = "SELECT count(*) FROM users WHERE role = 'residence'"
-        conn.query(sql,callback);
+        return query(sql,callback);
     },
     countTotalStaff : (id , callback)=>{
         console.log(id);
         const sql = "SELECT count(*) FROM staff WHERE cid = ?"
-        conn.query(sql,[id] , callback);
+        return query(sql,[id] , callback);
     },
 
 }
