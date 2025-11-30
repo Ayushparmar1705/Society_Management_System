e75d6d84760344107df280f345276a58e465c333
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-11-30 10:14:32 +0530

    Updated code in login controller file

--- backend/controller/User/Logincontroller.js
+++ backend/controller/User/Logincontroller.js
@@ -33,7 +33,7 @@ const loginController = {
             try {
                 const data = await mg.messages.create("sandbox91b71e47fdd04c2b9cb132573c009658.mailgun.org", {
                     from: "Urbanhome <postmaster@sandbox91b71e47fdd04c2b9cb132573c009658.mailgun.org>",
-                    to:  email,
+                    to: email,
                     subject: 'Urbanhome OTP. dont share to anyone',
                     text: `urbanhome otp ${otp}`
                 })
@@ -47,11 +47,21 @@ const loginController = {
             await createConnection.otpVerification.insert(otp, email);
             return res.status(200).send({ code: 200, message: "otp send in your main", result: result });
         }
-    
+
     },
     verifyOTP: async (req, res) => {
         const otpdata = req.body;
-        const sql = "SELECT * FROM users WHERE email = ?"
+        const sql = "SELECT * FROM users WHERE email = ?";
+        const result = await db.query(sql, [otpdata.email]);
+        if (result.length > 0) {
+            const verificationResult = await createConnection.otpVerification.verification(otpdata.email, otpdata.otp);
+            if (verificationResult === null) {
+                return res.status(500).send({ message: "Invalid OTP" });
+            }
+            else {
+                const token = jwt.sign({ id: result[0].uid }, process.env.JWT_SECRET, { expiresIn: "1h" });
+            }
+        }
         db.query(sql, [otpdata.email], async (err, result) => {
             if (err) {
                 return res.status(500).send({ message: err })
@@ -68,8 +78,7 @@ const loginController = {
                     }
                     else {
 
-                        const token = jwt.sign({ id: result[0].uid }, process.env.JWT_SECRET, { expiresIn: "1h" })
-                        const sid = jwt.sign({ sid: result[0].sid }, process.env.JWT_SECRET, { expiresIn: "1h" })
+                        const token = jwt.sign({ id: result[0].uid, sid: result[0].sid }, process.env.JWT_SECRET, { expiresIn: "1h" })
                         return res.status(200).send({ code: 200, message: "Otp verify succesfully", role: result[0].role, _token: token, sid: sid, society_id: result[0].society_id, flat_id: result[0].fid })
                     }
                 }
