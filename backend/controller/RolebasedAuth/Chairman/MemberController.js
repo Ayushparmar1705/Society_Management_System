f507879a45f820402920356c6e709dc83058f77a
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-11-30 16:59:44 +0530

    update the code for memberManagement and memberController

--- backend/controller/RolebasedAuth/Chairman/MemberController.js
+++ backend/controller/RolebasedAuth/Chairman/MemberController.js
@@ -2,43 +2,54 @@ const { memberManagement } = require("../../../model/RolebasedAuth/Chairman/memb
 
 const memberController = {
     // create the controller logic for manage the residence
-    residenceController: (req, res) => {
+    residenceController: async (req, res) => {
         const society_name = req.params.society_name;
-        
-        
+
+
         try {
-            
-            memberManagement.residenceManagement(society_name, (err, result) => {
-                if (err) {
-                    console.log(err);
-                    return res.status(500).send({ code: 500, message: err })
-                }
-                else {
-                    console.log(result);
-                    return res.status(200).send({ code: 200, message: result });
-                }
-            })
+
+
+            const result = memberManagement.res(society_name);
+            if (result) {
+                return res.status(200).send({ code: 200, message: result });
+            }
+
+            // memberManagement.residenceManagement(society_name, (err, result) => {
+            //     if (err) {
+            //         console.log(err);
+            //         return res.status(500).send({ code: 500, message: err })
+            //     }
+            //     else {
+            //         console.log(result);
+            //         return res.status(200).send({ code: 200, message: result });
+            //     }
+            // })
         }
         catch (err) {
-            console.log(err);
+            return res.status(500).send({ code: 500, message: err });
         }
     },
     // create the controller logic for approve  the member
-    ApproveController: (req, res) => {
+    ApproveController: async (req, res) => {
         const id = req.params.id;
         try {
             // call the Approve method from the model
-            memberManagement.Approve(id, (err, result) => {
-                if (err) {
-                    return res.status(500).send({ code: 500, message: err })
-                } else {
-                    if (result) {
-                        return res.status(200).send({ code: 200, message: "Member Approve succesfully" });
-                    }
-                }
-            })
+
+            const result = await memberManagement.Approve(id);
+            if (result) {
+                return res.status(200).send({ code: 200, message: "Member Approve succesfully" });
+            }
+            // memberManagement.Approve(id, (err, result) => {
+            //     if (err) {
+            //         return res.status(500).send({ code: 500, message: err })
+            //     } else {
+            //         if (result) {
+            //             return res.status(200).send({ code: 200, message: "Member Approve succesfully" });
+            //         }
+            //     }
+            // })
         } catch (err) {
-            console.log(err);
+            return res.status(500).send({ code: 500, message: err });
         }
     }
 
