d0a98d461da5dc58a3d742e59d33c3b41fc764fd
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-11-30 16:22:52 +0530

    Add the otp verification timer in hook and pagee

--- society_system/src/Hooks/UserHooks/OtpVerification.js
+++ society_system/src/Hooks/UserHooks/OtpVerification.js
@@ -3,43 +3,49 @@ import OtpVerificationpage from '../../Pages/Users/OtpVerificationpage'
 import { useState } from 'react'
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { ManageLogin } from '../../Api/User/ManageLogin';
-import {toast} from "react-toastify"
+import { toast } from "react-toastify"
+import { useEffect } from 'react';
 export default function OtpVerification() {
     const location = useLocation();
     const navigate = useNavigate();
     const email = location.state?.email
     const society_name = location.state?.society_name;
     const [otp, setOtp] = useState("");
-    const handleVerifyotp = async()=>{
-        const result = await ManageLogin.verifyOTP(email,otp)
-        
-        if(result.code === 200)
-        {
-            
-            if(result.role === 'chairman')
-            {
-                localStorage.setItem("token",result._token)
-                localStorage.setItem("society_name",society_name);
-                localStorage.setItem("society_id",result.sid);
+
+    const [timeLeft, setTimeleft] = useState(60);
+    const handleVerifyotp = async () => {
+        const result = await ManageLogin.verifyOTP(email, otp)
+
+        if (result.code === 200) {
+
+            if (result.role === 'chairman') {
+                localStorage.setItem("token", result._token)
+                localStorage.setItem("society_name", society_name);
+                localStorage.setItem("society_id", result.sid);
                 navigate("/dashboard")
                 toast.success(result.message);
             }
-            else
-            {
-                localStorage.setItem("flat_id",result.flat_id);
-                localStorage.setItem("role",result.role);
-                localStorage.setItem("sid",result.sid);
-                localStorage.setItem("token",result._token);
+            else {
+                localStorage.setItem("flat_id", result.flat_id);
+                localStorage.setItem("role", result.role);
+                localStorage.setItem("sid", result.sid);
+                localStorage.setItem("token", result._token);
                 navigate("/managevisitor");
                 toast.success(result.message);
             }
-          
+
         }
-        else
-        {
+        else {
             toast.error(result.message);
         }
 
+        useEffect(() => {
+
+            setInterval(() => {
+                setTimeleft(t => t - 1);
+            }, 1000);
+        }, [timeLeft]);
+
     }
-    return <OtpVerificationpage otp={otp} setOtp={setOtp} handleVerifyotp={handleVerifyotp}></OtpVerificationpage>
+    return <OtpVerificationpage timer={timeLeft} validotp={validotp} otp={otp} setOtp={setOtp} handleVerifyotp={handleVerifyotp}></OtpVerificationpage>
 }
--- society_system/src/Pages/Users/OtpVerificationpage.js
+++ society_system/src/Pages/Users/OtpVerificationpage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import OTPInput from "react-otp-input"
-export default function OtpVerificationpage({ otp, setOtp , handleVerifyotp}) {
+export default function OtpVerificationpage({ otp, setOtp , handleVerifyotp , timer}) {
     return (
         <div className='flex justify-center items-center h-screen'>
             <div className='rounded-xl text-lg p-2 bg-blue-500 h-3/5 w-3/5 flex flex-col text-white justify-center items-center'>
@@ -25,6 +25,7 @@ export default function OtpVerificationpage({ otp, setOtp , handleVerifyotp}) {
                     renderInput={(props) => <input {...props} />}
                 />
                 <button onClick={handleVerifyotp} className='p-2 text-black bg-white text-black rounded mt-10 w-2/5'>Verify</button>
+                <p>OTP valid only ${timer}</p>
             </div>
         </div>
     )
