16d88feeef1ac1da06aff684eecd04f327fcd4c4
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-01 11:29:04 +0530

    remove the callback from manage staff modal

--- backend/model/RolebasedAuth/Chairman/ManageStaffModel.js
+++ backend/model/RolebasedAuth/Chairman/ManageStaffModel.js
@@ -1,31 +1,31 @@
 const { query } = require("../../../config/dbConfig");
 
 const Managestaffmodel = {
-    Addstaff: (cid, data, callback) => {
+    Addstaff: (cid, data) => {
         const sql = "INSERT INTO staff(cid , name , email , phone , role) VALUES(?,?,?,?,?)"
-        return query(sql, [cid, data.name, data.email, data.phone, data.role], callback);
+        return query(sql, [cid, data.name, data.email, data.phone, data.role]);
     },
     Managestaff: (id) => {
         const sql = "SELECT * FROM staff WHERE cid = ?";
         return query(sql, [id]);
     },
-    DeleteStaff: (id, callback) => {
+    DeleteStaff: (id) => {
 
         const sql = "UPDATE staff SET is_active = 0 WHERE sid = ?"
-        return query(sql, [id], callback);
+        return query(sql, [id]);
     },
-    ActiveStaff: (id, callback) => {
+    ActiveStaff: (id) => {
 
         const sql = "UPDATE staff SET is_active = 1 WHERE sid = ?"
-        return query(sql, [id], callback);
+        return query(sql, [id]);
     },
-    getOneStaff: (id, callback) => {
+    getOneStaff: (id) => {
         const sql = "SELECT * FROM staff WHERE sid = ?";
-        return query(sql, [id], callback);
+        return query(sql, [id]);
     },
-    updateStaff : (id , data , callback)=>{
+    updateStaff : (id , data )=>{
         const sql = "UPDATE staff SET name = ? , email = ? , phone = ? , role = ? WHERE sid = ?";
-        return query(sql,[data.name , data.email , data.phone , data.role , id],callback);
+        return query(sql,[data.name , data.email , data.phone , data.role , id]);
     }
 }
 module.exports = { Managestaffmodel }
