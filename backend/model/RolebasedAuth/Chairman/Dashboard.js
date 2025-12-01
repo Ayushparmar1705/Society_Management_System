eed68904725a6f2b3fda63c71a75be6b8fb6a207
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-01 18:45:59 +0530

    change the sid to uid

--- backend/model/RolebasedAuth/Chairman/Dashboard.js
+++ backend/model/RolebasedAuth/Chairman/Dashboard.js
@@ -2,12 +2,12 @@ const { query } = require("../../../config/dbConfig");
 const Dashboardmodel = {
     countTotalMembers: (id) => {
         // count total residence
-        const sql = "SELECT count(*) FROM users WHERE role = 'residence' WHERE cid = ?"
+        const sql = "SELECT count(*) FROM users WHERE role = 'residence' WHERE uid = ?"
         return query(sql,[id]);
     },
     countTotalStaff : (id )=>{
 
-        const sql = "SELECT count(*) FROM staff WHERE cid = ?"
+        const sql = "SELECT count(*) FROM staff WHERE uid = ?"
         return query(sql,[id] );
     },
 
