b08565ba724f5d92b6bf90c754a72e77ff4933b0
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-02 21:20:18 +0530

    Updated code

--- society_system/src/Hooks/RoleBasedAuthhook/Residence/ManageVisitorHooks.js
+++ society_system/src/Hooks/RoleBasedAuthhook/Residence/ManageVisitorHooks.js
@@ -12,11 +12,13 @@ export default function ManageVisitor() {
 
     const fetchVisitor = async () => {
         const token = localStorage.getItem("token");
+        console.log("token" , jwtDecode(token));
         const sid = localStorage.getItem("sid");
         const decodedSid = jwtDecode(sid);
+        console.log("sid = ",decodedSid);
         const fid = localStorage.getItem("flat_id");
-        
-        const decodedToken = jwtDecode(token).id;     
+
+        console.log("fid = ",fid);
         const result = await ManageResidence.Managevisitor(decodedSid.sid,jwtDecode(fid).fid);
         console.log(result);
         setVisitor(result.message);
