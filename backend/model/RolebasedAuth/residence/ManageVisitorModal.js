240e6f0b7e37e6ab3d539862b35eec3b34758f81
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-02 22:13:42 +0530

    change the query to sql

--- backend/model/RolebasedAuth/residence/ManageVisitorModal.js
+++ backend/model/RolebasedAuth/residence/ManageVisitorModal.js
@@ -4,13 +4,13 @@ const { query } = require("../../../config/dbConfig");
 const ManageVisitormodal = {
     // Get visitors with flat details using JOIN
     getVisitorsByFlatId: (society_id,flat_id) => {
-        query = 'SELECT * FROM Addvisitor WHERE sid = ? AND flat_id = ?';
+        sql = 'SELECT * FROM Addvisitor WHERE sid = ? AND flat_id = ?';
         return db.query(sql, [society_id,flat_id]);
     },
 
     // Approve visitor example
     approveVisitor: (vid) => {
-        query = "UPDATE Addvisitor set is_approve = 1 WHERE visitor_id = ?"
+        sql = "UPDATE Addvisitor set is_approve = 1 WHERE visitor_id = ?"
         return db.query(sql, [vid]);
     }
 };
