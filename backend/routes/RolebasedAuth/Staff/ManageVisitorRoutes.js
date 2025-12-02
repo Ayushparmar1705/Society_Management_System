3243e80b97f01c1087f61fe72b3f46cc3e2d64c3
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-02 17:21:59 +0530

    Add code for manage visitor routes

--- backend/routes/RolebasedAuth/Staff/ManageVisitorRoutes.js
+++ backend/routes/RolebasedAuth/Staff/ManageVisitorRoutes.js
@@ -3,6 +3,6 @@ const { ManageVisitorController } = require("../../../controller/RolebasedAuth/S
 const router = express.Router();
 
 router.post("/add-visitor",ManageVisitorController.AddVisotor);
-router.get("/get-visitor/:society_id",ManageVisitorController.Managevisitor);
+router.get("/get-visitor/:sid/:fid",ManageVisitorController.Managevisitor);
 
 module.exports =  router
