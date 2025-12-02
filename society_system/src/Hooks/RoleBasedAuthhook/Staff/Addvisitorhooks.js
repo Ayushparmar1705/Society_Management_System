19f95a62ac70c55df71557556e08cfc84cc22924
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-02 14:57:02 +0530

    remove the society id

--- society_system/src/Hooks/RoleBasedAuthhook/Staff/Addvisitorhooks.js
+++ society_system/src/Hooks/RoleBasedAuthhook/Staff/Addvisitorhooks.js
@@ -25,7 +25,6 @@ export default function Addvisitorhooks() {
 
     }
     const getFlatCodeBySocietyName = async () => {
-        if (!societyId) return;
         try {
 
             const res = await Managesignup.getFlatsCode(decoded.sid);
