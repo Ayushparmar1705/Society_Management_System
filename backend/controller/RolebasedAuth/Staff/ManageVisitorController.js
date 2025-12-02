59ab1f9e1625c8696e3af4a1671948377191d816
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-02 15:28:58 +0530

    add the log in visitor controller backend file

--- backend/controller/RolebasedAuth/Staff/ManageVisitorController.js
+++ backend/controller/RolebasedAuth/Staff/ManageVisitorController.js
@@ -10,6 +10,7 @@ const ManageVisitorController = {
                 return res.status(200).send({ code: 200, message: "Visitor Add succesfully" });
             }
         } catch (err) {
+            console.log(err);
             return res.status(500).send({ code: 500, message: err });
         }
         // ManageVisitorModal.Addvisitor(data, (err, result) => {
