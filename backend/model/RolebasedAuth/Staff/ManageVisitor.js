ed25b4730962a24fc8d5a18bb8edd8330b124b2b
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-02 15:50:54 +0530

    change the query in ManageVisitor backend file

--- backend/model/RolebasedAuth/Staff/ManageVisitor.js
+++ backend/model/RolebasedAuth/Staff/ManageVisitor.js
@@ -2,8 +2,8 @@ const { query } = require("../../../config/dbConfig");
 
 const ManageVisitorModal = {
     Addvisitor: (data) => {
-        const sql = "INSERT INTO Addvisitor(sid , society_id , flat_id , visitor_name, visitor_phone, flat_code) VALUES (?,?, ?, ?,?,?)";
-        const params = [data.staff_id, data.society_id, data.visitor_name, data.visitor_phone, parseInt(data.flat_code)];
+        const sql = "INSERT INTO Addvisitor(sid , society_id , flat_id , visitor_name, visitor_phone) VALUES (?,?, ?, ?,?,?)";
+        const params = [data.staff_id, data.society_id, parseInt(data.flat_code), data.visitor_name, data.visitor_phone];
         return query(sql, params);
     },
     Managevisitor: (society_id) => {
