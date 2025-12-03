397645cf6cccbebd455f6d5c7e88d429e01c1fe3
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-03 15:40:14 +0530

    change the code of the memberManagement in backend file for super admin module

--- backend/model/MainAdmin/MembersManagementModal.js
+++ backend/model/MainAdmin/MembersManagementModal.js
@@ -2,12 +2,12 @@ const { query } = require("../../config/dbConfig");
 
 const MembersManagementModal = {
     viewMember: () => {
-        const sql = "SELECT * FROM users";
+        const sql = "SELECT u.username,u.email,u.phone,u.role,s.society_nam,f.flat_code FROM users AS u INNER JOIN Addsociety AS s ON u.sid = s.sid INNER JOIN Addflats AS f ON u.fid = f.fid";
         return query(sql);
     },
-    makeChairman:(id)=>{
-        const sql = "UPDATE users SET role = 'chairman' AND is_active = 1 WHERE uid = ?";
-        return query(sql,[id]);
+    makeChairman: (id) => {
+        const sql = "UPDATE users SET role = 'chairman' AND is_approve = 1 WHERE uid = ?";
+        return query(sql, [id]);
     }
 }
-module.exports = {MembersManagementModal}
+module.exports = { MembersManagementModal }
