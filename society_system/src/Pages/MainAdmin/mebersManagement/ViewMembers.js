41d865f796e3a1cc402639a9d330471e291d5042
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-03 16:01:22 +0530

    updated code in viewmember page for super admin

--- society_system/src/Pages/MainAdmin/mebersManagement/ViewMembers.js
+++ society_system/src/Pages/MainAdmin/mebersManagement/ViewMembers.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function ViewMembers({ result, loading, makeChairman }) {
   return (
     <div className='mt-[10px]'>
-      
+
       {loading ? (
         <div className='w-full flex justify-center items-center h-[300px]'>
           <img className='h-[100px] w-[100px] m-auto' src='/Assets/loading.gif' alt='Loading' />
@@ -17,28 +17,25 @@ export default function ViewMembers({ result, loading, makeChairman }) {
           <input
             type='search'
             placeholder='Search society by name'
-        
+
             className='outline-none rounded-[10px] border-2 border-gray-200 focus:border-blue-300 p-[10px] w-[400px] mx-auto block transition-all duration-300 ease-in-out'
           />
 
           {/* Cards */}
           <div className='flex flex-wrap w-full mt-10 gap-10 justify-center'>
             {result.map((result, index) => (
-              console.log(result),
+
               <div key={index}>
                 <div className='hover:scale-[1.04] hover:border-2 hover:border-gray-200 transition-transform duration-200 border-2 border-gray-100 p-4 rounded w-[350px] h-500 '>
 
-                  {/* Header */}
-                  
-                  {/* Info */}
                   <div className='flex w-full flex-col p-5 gap-2'>
-<div className={`text-white p-[10px] font-bold rounded`}>
-                    <div className='flex justify-between items-center'>
-                      <p>{result.username}</p>
-                      <User className="h-4 w-4 text-green-500 mr-3"></User>
-                    </div>
+                    <div className={`text-white p-[10px] font-bold rounded`}>
+                      <div className='flex justify-between items-center'>
+                        <p>{result.username}</p>
+                        <User className="h-4 w-4 text-green-500 mr-3"></User>
+                      </div>
 
-                  </div>
+                    </div>
 
                     <div className='flex items-center'>
                       <Mail className="h-4 w-4 text-blue-500 mr-3" />
@@ -56,7 +53,17 @@ export default function ViewMembers({ result, loading, makeChairman }) {
                         <p>{result.phone}</p>
 
                       </div>
+
                     </div>
+
+
+                    <div>
+                      <span className='text-sm text-gray-400'>Phone</span>
+                      <p>{result.society_name}</p>
+                      <p>{result.flat_code}</p>
+
+                    </div>
+
 
                     {/* Action Buttons */}
                     {result.role === "residence" ? (
