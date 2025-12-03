ce080e498a10d024109a2dc6ce22d94e237955e2
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-03 22:22:36 +0530

    change the code in viewmembers

--- society_system/src/Pages/MainAdmin/mebersManagement/ViewMembers.js
+++ society_system/src/Pages/MainAdmin/mebersManagement/ViewMembers.js
@@ -5,13 +5,13 @@ export default function ViewMembers({ result, loading, makeChairman }) {
   return (
     <div className='p-2'>
       <div className='p-50'>
-        <div className='flex p-5 bg-gray-100'>
+        <div className='flex p-5 bg-white'>
           {result.map((data) => (
-            <div className='bg-gray-50 rounded-xl shadow'>
-              <p>Name: {data.username}</p>
-              <p>Email: {data.email}</p>
-              <p>Phone: {data.phone}</p>
-              <p>Role: {data.residence}</p>
+            <div className='bg-gray-50 rounded-xl shadow p-10'>
+              <p className='p-2'>Name: {data.username}</p>
+              <p className='p-2'>Email: {data.email}</p>
+              <p className='p-2'>Phone: {data.phone}</p>
+              <p className='p-2'>Role: {data.residence}</p>
               <button onCanPlay={() => {
                 makeChairman(data.uid)
               }}>Make chairman</button>
