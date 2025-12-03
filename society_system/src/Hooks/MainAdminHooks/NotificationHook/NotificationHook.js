61e456f7b8a72d7425d6362cca5a04d1c348960d
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-03 18:49:33 +0530

    Add the notification hook in frontend

--- society_system/src/Hooks/MainAdminHooks/NotificationHook/NotificationHook.js
+++ society_system/src/Hooks/MainAdminHooks/NotificationHook/NotificationHook.js
@@ -1,6 +1,37 @@
-import React from 'react'
-import Notification from '../../../Pages/MainAdmin/Notification/Notification'
+import React, { useEffect, useState } from 'react'
+import Notification from '../../../Pages/Notification/Notification'
+import ManageNotification from '../../../Api/MainAdmin/ManageNotification'
+import { socket } from '../../../socketFile/Socket';
 
 export default function NotificationHook() {
-    return <Notification></Notification>
+    const [notification, setNotification] = useState([]);
+    const getNotification = async () => {
+        const result = await ManageNotification.manageNotification();
+        const data = await result.message;
+        setNotification(data);
+    }
+
+    useEffect(() => {
+        getNotification();
+        if (window.Notification && window.Notification.permission !== 'granted') {
+            window.Notification.requestPermission();
+        }
+        socket.on("new_notification", (data) => {
+            setNotification((prev) => [data, ...prev]);
+        })
+        const audio = new Audio("/Assets/notification.mp3");
+        audio.play();
+
+
+
+        toast.sucess("New notification recived!");
+
+        if (window.Notification && window.Notification.permission === 'granted') {
+            new window.Notification("New contact message", {
+                body: `${data.name} send a message`,
+                icon: '/Assets/notification_logo.jpg'
+            })
+        }
+    }, []);
+    return <Notification notification={notification}></Notification>
 }
