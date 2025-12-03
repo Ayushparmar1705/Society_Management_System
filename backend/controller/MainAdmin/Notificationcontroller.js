e7265aee8fb23770b60e205fe17df385dc639fc2
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-03 17:23:49 +0530

    updated code

--- backend/controller/MainAdmin/Notificationcontroller.js
+++ backend/controller/MainAdmin/Notificationcontroller.js
@@ -7,6 +7,7 @@ const NotificationController = {
                 return res.status(200).send({ code: 200, message: result });
             }
         } catch (err) {
+            console.log(err);
             return res.status(500).send({ code: 500, message: err });
         }
     }
