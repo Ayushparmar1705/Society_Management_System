80c4f829f9df4418ea52f33eb79557746bf8aa8f
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-03 13:27:55 +0530

    change the query in contactmodal

--- backend/model/Contact/ContactModal.js
+++ backend/model/Contact/ContactModal.js
@@ -3,7 +3,7 @@ const { query } = require("../../config/dbConfig");
 const ContactModal = {
     createContact: (data) => {
         const sql = "INSERT INTO Contact(name,email,society_name,address,purpose,message) VALUES(?,?,?,?,?,?)";
-        return query(sql, [data.name, data.email, data.society_name, data.address, data.purpose, data.message]);
+        return query(sql, [data.name, data.email, data.societyName, data.address, data.purpose, data.message]);
     }
 }
 module.exports = {ContactModal}
