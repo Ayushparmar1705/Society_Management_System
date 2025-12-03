7d296a01295f5b93778b5934f6e35282ada07d29
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-03 11:49:51 +0530

    updated code

--- society_system/src/Hooks/Contacthook/Contacthook.js
+++ society_system/src/Hooks/Contacthook/Contacthook.js
@@ -1,7 +1,8 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import Contact from '../../Pages/Contact/Contact'
 
 export default function Contacthook() {
-    <Contact></Contact>
+
+    return <Contact></Contact>
+
 }
