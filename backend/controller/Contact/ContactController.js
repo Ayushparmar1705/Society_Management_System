e09d11da4f6f734b968c74d36a2f517c0cec94d7
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-03 13:31:23 +0530

    remove the await from the req.body in contactcontroller

--- backend/controller/Contact/ContactController.js
+++ backend/controller/Contact/ContactController.js
@@ -2,7 +2,7 @@ const { ContactModal } = require("../../model/Contact/ContactModal")
 
 const ContactController = {
     createContact: async (req, res) => {
-        const data = await req.body;
+        const data =  req.body;
         console.log(data);
         try {
             const result = await ContactModal.createContact(data);
