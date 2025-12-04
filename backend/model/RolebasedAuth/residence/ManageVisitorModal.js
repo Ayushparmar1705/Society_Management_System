6ed1cf9580b12e364ac24ac102589b294a62c41b
Ayush_Parmar <ayushparmar1705@gmail.com>
2025-12-04 22:16:40 +0530

    change the query in manage visitor controller

--- backend/model/RolebasedAuth/residence/ManageVisitorModal.js
+++ backend/model/RolebasedAuth/residence/ManageVisitorModal.js
@@ -4,7 +4,7 @@ const { query } = require("../../../config/dbConfig");
 const ManageVisitormodal = {
     // Get visitors with flat details using JOIN
     getVisitorsByFlatId: (society_id,flat_id) => {
-        sql = 'SELECT * FROM Addvisitor WHERE society_id = ? AND flat_id = ?';
+        sql = 'SELECT * FROM Addvisitor AS av INNER JOIN Addflats AS af ON av.flat_id = af.flat_id WHERE af.society_id = ? AND af.flat_id = ?';
         return query(sql, [society_id,flat_id]);
     },
 
